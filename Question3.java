0bae5f17aa030aed60f94bec8a890246901190b3
PlatinumDL <64115176+PlatinumDL@users.noreply.github.com>
2020-04-25 16:39:21 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter Number: ");
+    int number = in.nextInt();
+    int square = (number*number);
+    System.out.println("The number squared is " + square);
     
   }
 }
